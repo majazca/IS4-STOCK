3ab608d3049b4fd1454073a37a85c53e3a0354f2
majazca <jazmin-10@hotmail.es>
2023-02-13 14:55:08 -0300

    cambios para produccion

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
     if (!defined("RUTA")) {
-        define("RUTA", "/var/www/html");
+        //define("RUTA", "/var/www/html");
+        define("RUTA","/home/majazca/actions-runner/uca/IS4-STOCK/IS4-STOCK/");
     }
--- php/conexion.php
+++ php/conexion.php
@@ -1,7 +1,7 @@
 <?php
     
     if (!defined("RUTA")) {
-        define("RUTA","/var/www/html");
+        define("RUTA","/home/majazca/actions-runner/uca/IS4-STOCK/IS4-STOCK/");
     }
 
     require RUTA.'/vendor/autoload.php';
